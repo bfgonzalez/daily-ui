8fa2ef3ce344d9f1b9853986a3f114c4c9ccc773
Bianca Gonzalez <33680528+bfgonzalez@users.noreply.github.com>
2018-01-13 21:48:33 +0800

    Minor revisions

--- day-1/js/index.js
+++ day-1/js/index.js
@@ -12,31 +12,37 @@ $(document).ready(function() {
       }
       
       // animation for #login-container
-      if ($("#login-container").css("bottom") == "30px") {
+      if ($("#login-container").css("bottom") == "20px") {
           $("#login-container").stop().animate({bottom: "1px"});
       } else {
-          $("#login-container").stop().animate({bottom: "30px"});
+          $("#login-container").stop().animate({bottom: "20px"});
       }
       
       // animation for button 
-      if ($("button").css("bottom") == "30px") {
+      if ($("button").css("bottom") == "20px") {
           $("button").stop().animate({bottom: "1px"});
       } else {
-          $("button").stop().animate({bottom: "30px"});
+          $("button").stop().animate({bottom: "20px"});
       }
       
-      // animation for #alt-login-container 
-      if ($("#alt-login-container").css("bottom") == "30px") {
-          $("#alt-login-container").stop().animate({bottom: "1px"});
+      // animation for #forgot-password 
+      if ($("#forgot-password").css("bottom") == "15px") {
+          $("#forgot-password").stop().animate({bottom: "1px"});
       } else {
-          $("#alt-login-container").stop().animate({bottom: "30px"});
+        $("#forgot-password").stop().animate({bottom: "15px"});
       }
+      // animation for #default-login 
+      if ($("#default-login").css("bottom") == "10px") {
+          $("#default-login").stop().animate({bottom: "1px"});
+      } else {
+          $("#default-login").stop().animate({bottom: "10px"});
+      } 
       
       var currentPage = $("#default-login").text();
       var switchPage = currentPage == "Already have an account? Log in!" ? "Don't have an account? Create one!" : "Already have an account? Log in!";
       
       var currentHeader = $("h1").text();
-      var switchHeader = currentHeader == "Welcome Back!" ? "Create an Account" : "Welcome Back!";
+      var switchHeader = currentHeader == "Welcome Back!" ? "Create Account" : "Welcome Back!";
       
       var currentButton = $("button").text();
       var switchButton = currentButton == "Log in" ? "Get Started" : "Log in";
