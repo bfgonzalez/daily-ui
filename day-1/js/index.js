85a4e28da297b96b3b6c56d45d167f6d31731b1d
Bianca Gonzalez <33680528+bfgonzalez@users.noreply.github.com>
2018-01-14 10:35:26 +0800

    Interface changes (animations)

--- day-1/js/index.js
+++ day-1/js/index.js
@@ -1,58 +1,52 @@
 $(document).ready(function() {
-    $("#forgot-password").hide();
-  
-      $("#default-login").on("click", function() {
-        $("#forgot-password").toggle(); 
-        
-        // animation for #email-container
-        if ($("#email-container").css("opacity") == "1") {
-            $("#email-container").stop().animate({opacity: "0"});
-        } else {
-            $("#email-container").stop().animate({opacity: "1"});
-        }
-        
-        // animation for #login-container
-        if ($("#login-container").css("bottom") == "20px") {
-            $("#login-container").stop().animate({bottom: "1px"});
-        } else {
-            $("#login-container").stop().animate({bottom: "20px"});
-        }
-        
-        // animation for button 
-        if ($("button").css("bottom") == "20px") {
-            $("button").stop().animate({bottom: "1px"});
-        } else {
-            $("button").stop().animate({bottom: "20px"});
-        }
-        
-        // animation for #forgot-password 
-        if ($("#forgot-password").css("bottom") == "15px") {
-            $("#forgot-password").stop().animate({bottom: "1px"});
-        } else {
-          $("#forgot-password").stop().animate({bottom: "15px"});
-        }
-        // animation for #default-login 
-        if ($("#default-login").css("bottom") == "10px") {
-            $("#default-login").stop().animate({bottom: "1px"});
-        } else {
-            $("#default-login").stop().animate({bottom: "10px"});
-        } 
-        
-        var currentPage = $("#default-login").text();
-        var switchPage = currentPage == "Already have an account? Log in!" ? "Don't have an account? Create one!" : "Already have an account? Log in!";
-        
-        var currentHeader = $("h1").text();
-        var switchHeader = currentHeader == "Welcome Back!" ? "Create Account" : "Welcome Back!";
-        
-        var currentButton = $("button").text();
-        var switchButton = currentButton == "Log in" ? "Get Started" : "Log in";
-       
-        $("#default-login").text(switchPage);
-        $("h1").text(switchHeader);
-        $("button").text(switchButton);
-         
-      });
-    
-  
+  $("#forgot-password").hide();
+
+    $("#default-login").on("click", function() {
+      $("#forgot-password").toggle(); 
+      
+      // animation for #email-container
+      if ($("#email-container").css("opacity") == "1") {
+          $("#email-container").animate({opacity: "0"});
+      } else {
+          $("#email-container").animate({opacity: "1"});
+      }
+      
+      // animation for #login-container
+      if ($("#login-container").css("bottom") == "30px") {
+          $("#login-container").animate({bottom: "0"});
+      } else {
+          $("#login-container").animate({bottom: "30px"});
+      }
+      
+      // animation for button 
+      if ($("button").css("bottom") == "30px") {
+          $("button").animate({bottom: "0"});
+      } else {
+          $("button").animate({bottom: "30px"});
+      }
+      
+      // animation for #alt-login-container 
+      if ($("#alt-login-container").css("bottom") == "30px") {
+          $("#alt-login-container").animate({bottom: "0"});
+      } else {
+          $("#alt-login-container").animate({bottom: "30px"});
+      }
+      
+      var currentPage = $("#default-login").text();
+      var switchPage = currentPage == "Already have an account? Log in!" ? "Don't have an account? Create one!" : "Already have an account? Log in!";
+      
+      var currentHeader = $("h1").text();
+      var switchHeader = currentHeader == "Welcome Back!" ? "Create an Account" : "Welcome Back!";
+      
+      var currentButton = $("button").text();
+      var switchButton = currentButton == "Log in" ? "Get Started" : "Log in";
      
-  });
+      $("#default-login").text(switchPage);
+      $("h1").text(switchHeader);
+      $("button").text(switchButton);
+       
+    });
+  
+
+   
+});
